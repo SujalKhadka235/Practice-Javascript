7cd8f0dc671e27826126cec6242d65469321d885
Sujal Khadka <sujalkhadka235@gmail.com>
2023-12-19 17:32:57 +0545

    Adding the difference between ES5 and ES6 in you.js

---
+++ you.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+//ES5
+var person = "sujal";
+person = "hari";
+//ES6
+//ALLOWS YOU TO REASSIGN THE VARIABLE
+let person1 = "ram";
+person1 = "hari";
+const person2 = "shyam";
+//Proudces error when exectued beacuse const value cannot be altered later on
+//person1='laxman';
+console.log(person2);
