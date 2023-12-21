cd8316ce725a1afa863b821cdaaa192025681e58
Sujal Khadka <sujalkhadka235@gmail.com>
2023-12-21 11:58:11 +0545

    added a basic calculator

---
+++ caculator.js
@@ -0,0 +1,26 @@
+let a = 0;
+let b = 0;
+function add() {
+  a = Number(prompt("Enter the first number"));
+  b = Number(prompt("Enter the second number"));
+  let sum = a + b;
+  alert("The sum of the two numbers is " + sum);
+}
+function subtract() {
+  a = Number(prompt("Enter the first number"));
+  b = Number(prompt("Enter the second number"));
+  let sub = a - b;
+  alert("The subtraction of the two numbers is " + sub);
+}
+function divide() {
+  a = Number(prompt("Enter the first number"));
+  b = Number(prompt("Enter the second number"));
+  let div = a / b;
+  alert("The divison of the two numbers is " + div);
+}
+function multiply() {
+  a = Number(prompt("Enter the first number"));
+  b = Number(prompt("Enter the second number"));
+  let mul = a * b;
+  alert("The multiplication of the two numbers is " + mul);
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+// function g() {
+//   let x = 20;
+//   let y = 30;
+//   if (x > 100 || y < 50) {
+//     alert("hello");
+//     console.log("hello");
+//   }
+// }
+const gender = prompt("Enter your gender(m/n");
+if (gender === "m") {
+  alert("You are male");
+} else {
+  alert("you are female");
+}
+const value = Number(prompt("Enter your number"));
+switch (value) {
+  case 1:
+    console.log("1");
+    break;
+  default:
+    console.log("else");
+}
