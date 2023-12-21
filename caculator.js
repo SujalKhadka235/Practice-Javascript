12bb2771254d7d696c1823ec95cb1b7dde277e95
Sujal Khadka <sujalkhadka235@gmail.com>
2023-12-21 17:25:40 +0545

    Loops and ascii value logic

--- caculator.js
+++ caculator.js
@@ -4,23 +4,27 @@ function add() {
   a = Number(prompt("Enter the first number"));
   b = Number(prompt("Enter the second number"));
   let sum = a + b;
+  console.log(sum);
   alert("The sum of the two numbers is " + sum);
 }
 function subtract() {
   a = Number(prompt("Enter the first number"));
   b = Number(prompt("Enter the second number"));
   let sub = a - b;
+  console.log(sub);
   alert("The subtraction of the two numbers is " + sub);
 }
 function divide() {
   a = Number(prompt("Enter the first number"));
   b = Number(prompt("Enter the second number"));
   let div = a / b;
+  console.log(div);
   alert("The divison of the two numbers is " + div);
 }
 function multiply() {
   a = Number(prompt("Enter the first number"));
   b = Number(prompt("Enter the second number"));
   let mul = a * b;
+  console.log(mul);
   alert("The multiplication of the two numbers is " + mul);
 }
---
+++ problems.js
@@ -0,0 +1,39 @@
+/*let x = 100;
+let y = 50;
+function check() {
+  if ((x = 100 || x + y == 100)) {
+    console.log("true");
+  }
+}
+check();*/
+//A program to sum values of an array
+//find the max elements in an array
+/*
+let a = [1, 2, 18, 10];
+let i = 0;
+let max = 0;
+max = a[0];
+for (i = 1; i < 4; i++) {
+  if (a[i] > max) {
+    max = a[i];
+  }
+}
+console.log(max);*/
+//find the min element of array
+/*
+let a = [11, 7, 18, 10];
+let i = 0;
+let min = 0;
+min = a[0];
+for (i = 1; i < 4; i++) {
+  if (a[i] < min) {
+    min = a[i];
+  }
+}
+console.log(min);*/
+let num1 = Number(prompt("Enter your number")); // "55"
+if (isNaN(num1)) {
+  alert("you entered a string");
+} else {
+  alert("You entered a number");
+}
